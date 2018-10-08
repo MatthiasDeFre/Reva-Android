72f96e9a1214bda15e1f79698da07ba63fee7edd
Jensdewulf <jens.dewulf.w4492@student.hogent.be>
2018-10-08 11:49:39 +0200

    basic inlogscherm

--- app/src/main/java/com/example/beardwulf/reva/MainActivity.kt
+++ app/src/main/java/com/example/beardwulf/reva/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.beardwulf.reva
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var txtCode: TextView
+    //   private val txtCode = findViewById<TextView>(R.id.input) as TextView
+    private lateinit var btnLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById<Button>(R.id.btnLogin)
+        txtCode = findViewById<TextView>(R.id.input)
+
+        btnLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    fun login() {
+        val tekst: String
+        if(txtCode.text.toString().equals("1234")) {
+            tekst = "Welcome, Jens"
+        }
+        else {
+            tekst = "Code niet correct"
+        }
+        Toast.makeText(this@MainActivity, tekst , Toast.LENGTH_SHORT).show()
     }
 }
