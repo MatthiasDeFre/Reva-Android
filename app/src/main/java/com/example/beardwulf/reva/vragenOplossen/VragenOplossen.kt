4669136411b3bed3149b88ed7efc47af2b89f0a3
Cedric Arickx <cedric.arickx@gmail.com>
2018-10-20 16:06:41 +0200

    Vraag invullen scherm basis

---
+++ app/src/main/java/com/example/beardwulf/reva/vragenOplossen/VragenOplossen.kt
@@ -0,0 +1,17 @@
+package com.example.beardwulf.reva.vragenOplossen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beardwulf.reva.R
+
+class VragenOplossen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vragen_oplossen)
+    }
+
+    fun vulIn() {
+
+    }
+}
