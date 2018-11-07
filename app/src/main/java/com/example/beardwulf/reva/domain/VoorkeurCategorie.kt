3a3859a829a97d1cdda9d3e025f88d7d25f2d5e5
Karel Heyndrickx <karel.heyndrickx.y7711@student.hogent.be>
2018-11-07 11:50:37 +0100

    Choose categories -> fragment

--- app/src/main/java/com/example/beardwulf/reva/domain/VoorkeurCategorie.kt
+++
@@ -1,9 +0,0 @@
-package com.example.beardwulf.reva.domain
-
-class VoorkeurCategorie {
-    var naam: String = ""
-
-    constructor(categorie: String) {
-        this.naam = categorie
-    }
-}
--- app/src/main/java/com/example/beardwulf/reva/fragments/registreren/RegistreerGroep.kt
+++ app/src/main/java/com/example/beardwulf/reva/fragments/registreren/RegistreerGroep.kt
@@ -3,29 +3,20 @@ package com.example.beardwulf.reva.fragments.registreren
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
-import com.example.beardwulf.reva.Endpoint
 import com.example.beardwulf.reva.R
-import com.example.beardwulf.reva.RetrofitClientInstance
-import com.example.beardwulf.reva.activities.VoorkeurCategorieen
-import com.example.beardwulf.reva.domain.Group
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.beardwulf.reva.activities.registreren.Registreren
 import kotlinx.android.synthetic.main.fragment_registreer_groep.*
-import org.jetbrains.anko.find
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class RegistreerGroep : Fragment() {
     /**
      * Registreer de gegevens van de groep
      * groepsnaam, groepsleden
      */
+    lateinit var parent: Registreren
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,14 +25,16 @@ class RegistreerGroep : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = inflater.inflate(R.layout.fragment_registreer_groep, container, false)
+
+        parent = (activity as Registreren)
+
         return view
     }
 
     override fun onResume() {
         super.onResume()
         cmdNaarCategorie.setOnClickListener {
-            var intent = Intent(activity, VoorkeurCategorieen::class.java)
-            startActivity(intent)
+            parent.setFragment(RegisterCategories.newInstance())
         }
     }
 
