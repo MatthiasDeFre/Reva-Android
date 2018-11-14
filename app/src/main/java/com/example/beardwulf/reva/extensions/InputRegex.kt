cd768790f6f4ea1460532bac6e110a512bb6a199
Jelle <geers.jelle@gmail.com>
2018-11-14 13:57:58 +0100

    Als gebruiker wil ik dat mijn gegevens correct moeten zijn finished

--- app/src/main/java/com/example/beardwulf/reva/extensions/InputRegex.kt
+++ app/src/main/java/com/example/beardwulf/reva/extensions/InputRegex.kt
@@ -2,7 +2,7 @@ package com.example.beardwulf.reva.extensions
 
 class InputRegex {
     companion object {
-        fun controleerLettersCijfers(input: String) : Boolean = "[a-zA-Z0-9]+".toRegex().matches(input)
+        fun controleerLettersCijfers(input: String) : Boolean = "[a-zA-Z0-9 ]+".toRegex().matches(input)
         fun controleerLetters(input:String):Boolean="[a-zA-Z]+".toRegex().matches(input)
     }
 
